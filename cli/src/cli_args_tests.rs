23d72e1c3cb6757f1770146b3bd942ef2070d4a9
supertypo <suprtypo@pm.me>
2026-05-29 01:40:10 +0200

    Removed tests locking default values

--- cli/src/cli_args_tests.rs
+++ cli/src/cli_args_tests.rs
@@ -15,42 +15,9 @@ fn parses_required_network_id() {
 }
 
 #[test]
-fn defaults_match_spec() {
-    let cli = parse(&["--network-id", "kaspa-mainnet"]);
-    assert_eq!(cli.crawler.threads, 8);
-    assert_eq!(cli.crawler.probes_per_peer, 3);
-    assert_eq!(cli.crawler.probe_timeout, Duration::from_secs(4));
-    assert_eq!(cli.crawler.probe_tick, Duration::from_secs(5));
-    assert_eq!(cli.crawler.stale_good, Duration::from_secs(30 * 60));
-    assert_eq!(cli.crawler.stale_bad, Duration::from_secs(2 * 60 * 60));
-    assert_eq!(cli.crawler.dead_after, Duration::from_secs(7 * 24 * 60 * 60));
-    assert_eq!(cli.dns.dns_listen, vec!["0.0.0.0:53".parse().unwrap(), "[::]:53".parse().unwrap()]);
-    assert_eq!(cli.http.http_listen, "127.0.0.1:8080");
-    assert_eq!(cli.http.post_rate_limit, 5);
-    assert_eq!(cli.http.rate_limit_window, Duration::from_secs(60));
-    assert!(cli.http.api_key.is_none());
-    assert!(cli.http.allowed_origins.is_empty());
-    assert!(!cli.dns_enabled());
-    assert!(!cli.crawler.strict_port);
-    assert_eq!(cli.datadir, "data");
-    assert_eq!(cli.http.api_prefix, "/api");
-    assert_eq!(cli.logging.log_level, "warn,simply_kaspa_dnsseeder=info");
-    assert!(!cli.logging.log_no_color);
-    assert_eq!(cli.stats_interval, Duration::from_secs(60));
-    assert!(cli.dns.min_protocol_version.is_none());
-    assert!(cli.dns.min_user_agent.is_none());
-}
-
-#[test]
 fn threads_zero_rejected() {
     let res = CliArgs::try_parse_from(["simply-kaspa-dnsseeder", "--network-id", "kaspa-mainnet", "--threads", "0"]);
     assert!(res.is_err(), "--threads 0 must be rejected");
-}
-
-#[test]
-fn probes_per_peer_defaults_to_three() {
-    let cli = parse(&["--network-id", "kaspa-mainnet"]);
-    assert_eq!(cli.crawler.probes_per_peer, 3);
 }
 
 #[test]
